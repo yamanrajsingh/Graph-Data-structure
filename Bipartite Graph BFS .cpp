051379b9eb81f9ee6cadf40b4ade5640e368ae2d
Yaman Raj Singh <yamanrajsingh07@gmail.com>
2024-07-27 02:55:30 +0530

    commit

---
+++ Bipartite Graph BFS .cpp
@@ -0,0 +1,47 @@
+
+
+class Solution
+{
+public:
+    bool check(int start, vector<int> adj[], int color[])
+    {
+        queue<int> q;
+        q.push(start);
+        color[start] = 0;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto it : adj[node])
+            {
+                if (color[it] == -1)
+                {
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+                else if (color[it] == color[node])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool isBipartite(int V, vector<int> adj[])
+    {
+        // Code here
+        int color[V];
+        for (int i = 0; i < V; i++)
+            color[i] = -1;
+
+        for (int i = 0; i < V; i++)
+        {
+            if (color[i] == -1)
+            {
+                if (check(i, adj, color) == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
